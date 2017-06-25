9a82268c6cd94c152ddb03743407f262bb1491fc
Malachai Frazier <malachaifrazier@gmail.com>
2017-06-25 14:19:18 -0400

    rails upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rims::Application.routes.draw do
-	get 'errors/not_found'
-	get 'errors/internal_server_error'
-	get "welcome/index"
-	get "/items/results" => "items#results"
-	get "/items/csv" => "items#csv"
-	get "/items/update_departments", as: "update_departments"
-	match "/404", :to => "errors#not_found", :via => :all
-	match "/500", :to => "errors#internal_server_error", :via => :all
-	devise_for :users
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  get "welcome/index"
+  get "/items/results" => "items#results"
+  get "/items/csv" => "items#csv"
+  get "/items/update_departments", as: "update_departments"
 
-	root :to => 'welcome#index'
-	resources :items do
-		collection { post :import}
-	end
-	resources :users
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
+  devise_for :users
+
+  root :to => 'welcome#index'
+  resources :items do
+    collection { post :import}
+  end
+  resources :users
 end
