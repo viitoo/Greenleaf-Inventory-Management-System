eded356cbabcb4ed076f740c187336120f60cea0
Malachai Frazier <malachaifrazier@gmail.com>
2017-07-03 18:53:17 -0400

    user_spec refactored, added Shoulda::Matchers to rails_helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,109 +1,109 @@
-# spec/models/user.rb
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    FactoryGirl.create(:user).should be_valid
+  let(:user) { create(:user) }
+
+  it { should_not allow_value('blah').for(:email) }
+  it { should allow_value('bigballscaptain@testicles.me').for(:email) }
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:password) }
+
+  it { should respond_to(:encrypted_password) }
+  it { should respond_to(:reset_password_token) }
+  it { should respond_to(:reset_password_sent_at) }
+  it { should respond_to(:remember_created_at) }
+  it { should respond_to(:sign_in_count) }
+  it { should respond_to(:current_sign_in_at) }
+  it { should respond_to(:last_sign_in_at) }
+  it { should respond_to(:current_sign_in_ip) }
+  it { should respond_to(:last_sign_in_ip) }
+  it { should respond_to(:role) }
+
+  describe 'validation of username' do
+    subject { User.new }
+    it { should validate_presence_of(:username) }
   end
 
-  it "is invalid without a email" do
-    FactoryGirl.build(:user, email: nil).should_not be_valid
+  describe 'validation of email' do
+    subject { FactoryGirl.build(:user, email: nil) }
+    it { should validate_presence_of(:email) }
   end
 
-  it "is invalid without a password"  do
-    FactoryGirl.build(:user, password: nil).should_not be_valid
+  describe 'valid Model' do
+    it 'should be an instance of User Model' do
+      expect(user).to be_an_instance_of(User)
+    end
+
+    it 'should have a password length btwn 6..40' do
+      expect(user.password.length).to be > 6
+      expect(user.password.length).to be < 40
+    end
+
+    it 'should validate_uniqueness_of email' do
+      faked_password = Faker::Internet.password
+      expect {
+        create(:user, email: user.email, password: faked_password,
+         password_confirmation: faked_password)
+        }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 
-# nkotb
-before(:each) do
-  @attr = {
-    :email => "user@example.com",
-    :password => "changeme",
-    :password_confirmation => "changeme"
-  }
-end
+  # nkotb
+  before(:each) do
+    @attr = {
+      :email => "user@example.com",
+      :password => "changeme",
+      :password_confirmation => "changeme"
+    }
+  end
 
   it "should create a new instance given a valid attribute" do
     User.create!(@attr)
   end
 
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
-
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+      valid_email_user = User.new(@attr.merge(email: address))
+      expect(valid_email_user).to be_valid
     end
   end
 
   it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
+      invalid_email_user = User.new(@attr.merge(email: address))
+      expect(invalid_email_user).to_not be_valid
     end
   end
 
   it "should reject duplicate email addresses" do
     User.create!(@attr)
     user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
+    expect(user_with_duplicate_email).to_not be_valid
   end
 
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
+    User.create!(@attr.merge(email: upcased_email))
     user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-
-  describe "passwords" do
-    before(:each) do
-      @user = User.new(@attr)
-    end
-
-    it "should have a password attribute" do
-      @user.should respond_to(:password)
-    end
-
-    it "should have a password confirmation attribute" do
-      @user.should respond_to(:password_confirmation)
-    end
+    expect(user_with_duplicate_email).to_not be_valid
   end
 
   describe "password validations" do
     it "should require a password" do
-      User.new(@attr.merge(:password => "", :password_confirmation => "")).
-        should_not be_valid
+      expect(User.new(@attr.merge(password: "", password_confirmation: ""))).to_not be_valid
     end
 
     it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).
-        should_not be_valid
+      expect(User.new(@attr.merge(password_confirmation: "invalid"))).to_not be_valid
     end
 
     it "should reject short passwords" do
       short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short)
-      User.new(hash).should_not be_valid
-    end
-  end
-
-  describe "password encryption" do
-    before(:each) do
-      @user = User.create!(@attr)
-    end
-
-    it "should have an encrypted password attribute" do
-      @user.should respond_to(:encrypted_password)
-    end
-
-    it "should set the encrypted password attribute" do
-      @user.encrypted_password.should_not be_blank
+      hash = @attr.merge(password: short, password_confirmation: short)
+      expect(User.new(hash)).to_not be_valid
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,17 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+require 'shoulda/matchers'
+::Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
