526372cdb82d49e475e8edd2eadbdf03b3fbf807
Malachai Frazier <malachaifrazier@gmail.com>
2017-07-03 19:13:41 -0400

    added password and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, if: :new_record?
 
+  validates :password, presence: true, on: :create, length: { within: 6..40 }
+  validates :email, presence: true, uniqueness: true,
+            format: {
+              with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+            }
+
+
   def set_default_role
     self.role ||= :user
   end
