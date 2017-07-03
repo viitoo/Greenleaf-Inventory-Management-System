ae9f27802b74928678cd0e8bed34014bce75b9af
Malachai Frazier <malachaifrazier@gmail.com>
2017-07-03 14:24:31 -0400

    cleanup

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,6 @@ class Item < ActiveRecord::Base
   self.per_page = 25
   belongs_to :item
 
-  # attr_accessible :description, :property, :device_name, :serial_number, :product_model_number, :quantity, :category, :department, :warrenty_date
-
   def self.search(search)
     where("serial_number LIKE ? OR device_name LIKE ?", "%#{search}%", "%#{search}%")
   end
